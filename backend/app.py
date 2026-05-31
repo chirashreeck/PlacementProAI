a2b5331a26c26832eb6d3fb5ff034bab95ee2a42
Chirashree <chirashreeck@gmail.com>
2026-05-31 21:39:37 +0530

    add path

--- backend/app.py
+++ backend/app.py
@@ -549,6 +549,9 @@ from flask import send_from_directory
 @app.route("/")
 def home():
     return send_from_directory("../frontend", "index.html")
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('../frontend', path)
 if __name__ == "__main__":
     print("Launching Flask Server...")
     app.run(
